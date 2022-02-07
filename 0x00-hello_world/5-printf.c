838d487d7a2d9b6b4e4052980fd5a79d55ab9053
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-02-06 20:18:38 -0500

    task5 Print result followed by a new line

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - print result
+* followed by a new line
+* Return: print 0
+*/
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
