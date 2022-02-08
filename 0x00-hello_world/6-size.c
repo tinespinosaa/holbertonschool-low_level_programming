5d9ccb4c12f9142a4fe6d8dbb26fc4fca59c5388
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-02-07 19:12:05 -0500

    task6 prints numbers of base 10 and newline 02

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,14 +2,15 @@
 /**
 * main - print the size of various types
 * on the computer it is compiled and run on
-*Return: none
+* Return: none
 */
 int main(void)
 {
-	printf("Size of a char: 1 byte(s) \n");
-	printf("Size of an int: 4 byte(s) \n");
-	printf("Size of a long int: %ld byte(s) \n", sizeof(long int));
-	printf("Size of a long long int: 8 byte(s) \n");
-	printf("Size of a float: 4 byte(s) \n");
-}
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
+	return (0);
+}
