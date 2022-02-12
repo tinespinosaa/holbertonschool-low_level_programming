e37c6c4b032c5cfbd7abdaa3a48e15c1858403ee
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-02-12 00:01:53 -0500

    task1 Function that prints the alphabet in lowercase followed by a new linewq

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_alphabet - writes the alphabet in lowercase
+* Return: print 0
+*/
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i > 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
