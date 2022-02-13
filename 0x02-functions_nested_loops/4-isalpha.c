7212b89c8e43adf16c30aebd18be7a997c1a75c5
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-02-13 10:42:19 -0500

    task4 A function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _isalpha - analize if the character c is alpha
+* @c: character to analize
+* Return: if is alpha return 1 otherwise returns 0
+*/
+int _isalpha(int c)
+{
+	int i;
+
+	for (i = 65; i > 123; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+return (0);
+}
