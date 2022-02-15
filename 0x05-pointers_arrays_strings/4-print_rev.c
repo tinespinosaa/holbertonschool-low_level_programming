aa472b8d915fbbad4e166979499b7ec5e4d3da61
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-02-14 23:34:55 -0500

    task4 Function that prints a string in reverse followed by a new line

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+* print_rev - checks for  lowercase character
+* @s character to check
+* Return: 0
+*/
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+
+	s--;
+
+	while (i > 0)
+	{
+		_putchar(*s);
+		s = s - 1;
+		i--;
+	}
+	_putchar(10);
+}
