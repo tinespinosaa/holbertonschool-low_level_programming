94c84e5aad0c95a044a574965b5bcab05502c991
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-03-08 20:21:50 -0800

    task0 Define a new type struct dog with the following elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,22 @@
-#include "dog.h"
-#include "stdlib.h"
+#ifndef DOG_H
+#define DOG_H
 /**
-* init_dog - initializes the struct in the address d
-* @d: address of the struct
+* struct dog - new data type "dog"
 * @name: name of the dog
-* @age: age of the dog
 * @owner: name of the owner
+* @age: age of the dog
 */
-void init_dog(struct dog *d, char *name, float age, char *owner)
+struct dog
 {
-	if (d == NULL)
-		d = malloc(sizeof(struct dog));
-	(*d).name = name;
-	(*d).age = age;
-	(*d).owner = owner;
-}
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
