90d2fa09905c333617556d790021cf1fbaf7c68e
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-03-08 21:07:37 -0800

    task4 Write a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,16 +1,43 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
-* print_dog -  function that prints a struct dog
-* @d: dog'structure.
-* Description: print the dog' struct
+* new_dog - creates and initializes a new instance of dog_t
+* @name: name of the dog
+* @age: age of the dog
+* @owner: name of the owner
+* Return: address of new instance of dog_t
 */
-void print_dog(struct dog *d)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	if (d != NULL)
+	struct dog *d;
+	int i, j;
+
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	d->name = malloc(i + 1);
+	if (d->name == NULL)
 	{
-		printf("Name: %s\n", (*d).name ? (*d).name : "(nil)");
-		printf("Age: %f\n", (*d).age ? (*d).age : 0);
-		printf("Owner: %s\n", (*d).owner ? (*d).owner : "(nil)");
+		free(d);
+		return (NULL);
 	}
+	for (j = 0; j < i; j++)
+		d->name[j] = name[j];
+	d->name[j] = '\0';
+	d->age = age;
+	for (i = 0; owner[i] != '\0'; i++)
+		;
+	d->owner = malloc(i + 1);
+	if (d->owner == NULL)
+	{
+		free(d);
+		free(d->name);
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+		d->owner[j] = owner[j];
+	d->owner[j] = '\0';
+	return (d);
 }
