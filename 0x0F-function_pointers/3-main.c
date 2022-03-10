cc7c54b1bbdcde280da7f7fa034ead6e8694de1e
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-03-10 10:06:07 -0800

    This file should contain your main function only

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,27 +1,41 @@
-#include <stdio.h>
 #include "3-calc.h"
-#include <stdlib.h>
 /**
-  * main - function main
-  * @argc:  ...
-  * @argv: ...
-  * Return: ....
-  */
+ * main- isdigit
+ * @argc: integer
+ * @argv: integer
+ * Return: prints alphabet
+ */
 int main(int argc, char *argv[])
 {
-	int (*op_func)(int, int);
+	int num1;
+	int num2;
+	int result;
 
+	num1 = 0;
+	num2 = 0;
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	op_func = get_op_func(argv[2]);
-	if (op_func == NULL)
+	if (argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '*'
+	    && argv[2][0] != '/' && argv[2][0] != '%')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	printf("%d\n", op_func(atoi(argv[1]), atoi(argv[3])));
+	if (argv[2][1] == '\0')
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[3]);
+		result = (*get_op_func(argv[2]))(num1, num2);
+		printf("%d\n", result);
+
+	} else
+	{
+
+		printf("Error\n");
+		exit(99);
+	}
 	return (0);
 }
