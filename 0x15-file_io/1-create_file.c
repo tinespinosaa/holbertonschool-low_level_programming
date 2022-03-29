5e0cdda5bb3274cf114ce04b83a92925ae2ff416
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-03-28 21:43:00 -0700

    task1 Function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 * create_file - creates a file
 * @filename: the name of the file to create
 * @text_content: char pointer
-* Return: 1 
+* Return: 1
 */
 int create_file(const char *filename, char *text_content)
 {
