34bb9af5392c77d2cae8730f161ec09e38dccf7a
tinespinosaa <agustinespinozasaavedra@gmail.com>
2022-09-05 23:47:22 -0500

    task11 Write a function that searches for a value in a sorted array of integers

---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+/**
+ * advanced_binary - searches for a value in a sorted array of integers using
+ * the Binary search algorithm with recursion
+ * @array: a pointer to the first element of the array to search in
+ * @size: the number of elements of the array
+ * @value: the value to find
+ * Return: the first index where value is located, or -1 if value is not
+ * present in array or if array is NULL
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	;
+}
